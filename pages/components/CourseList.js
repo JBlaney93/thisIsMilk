3dbea28e7a85b88229d757cac2f67ee678c635f2
JBlaney93 <jamesblaney93@gmail.com>
2023-04-30 20:29:52 +0100

    toggle incomplete modules button now working! huzzah!

--- pages/components/CourseList.js
+++ pages/components/CourseList.js
@@ -1,75 +1,103 @@
-import React, { useEffect } from 'react';
-import styles from '@/styles/Home.module.css'
+import React, { useEffect } from "react";
+import styles from "@/styles/Home.module.css";
 
 export default function CourseList({ courses }) {
+  // console.log('props', courses)
 
-    // console.log('props', courses)
+  const [showAllIncomplete, setShowAllIncomplete] = React.useState(false);
+  const modules = showAllIncomplete
+    ? courses.modules.map((module) => {
+        const incompleteActivities = module.activities.filter(
+          (activity) => !activity.completed
+        );
+        return {
+          ...module,
+          activities: incompleteActivities,
+        };
+      })
+    : courses.modules;
+  console.log(modules);
 
-    const [showAllIncomplete, setShowAllIncomplete] = React.useState(false);
+  const showAllIncompleteModules = () => {
+    let incompleteModules = courses.modules.map((module) => {
+      const incompleteActivities = module.activities.filter(
+        (activity) => !activity.completed
+      );
+      return {
+        ...module,
+        activities: incompleteActivities,
+      };
+    });
 
-    const showAllIncompleteModules = () => {
-        let incompleteModules = [];
-        courses.modules.forEach((module) => {
-            const incompleteActivities = module.activities.filter(
-                (activity) => !activity.completed
-            );
-            incompleteModules.push({
-                ...module,
-                activities: incompleteActivities
-            }) 
-        })
+    console.log(incompleteModules);
+  };
 
-        console.log(incompleteModules)
-    }
-    
-    return (
-        <div className={styles.pagewrapper}>
-        <h1>Course Information</h1>
-        <h2>{courses.name} ({courses.id})</h2>
+  return (
+    <div className={styles.pagewrapper}>
+      <h1>Course Information</h1>
+      <h2>
+        {courses.name} ({courses.id})
+      </h2>
 
-        <div className={styles.description}>
+      <div className={styles.description}>
         <article>{courses.description}</article>
-            <ul>
-                <h3>Learning Objectives</h3>
-                {courses.learningObjectives?.map((learningObjective, index) => (
-                <li key={index}>
-                    {learningObjective}
-                </li>
-                ))}
-            </ul>
-        </div>
-            
         <ul>
-            <h2>Modules:</h2>
-            {/* <button onClick={showAllIncompleteModules}>Show All Incomplete Modules</button> */}
+          <h3>Learning Objectives</h3>
+          {courses.learningObjectives?.map((learningObjective, index) => (
+            <li key={index}>{learningObjective}</li>
+          ))}
+        </ul>
+      </div>
 
-            {courses.modules?.sort((a,b) => a.order - b.order).map((module, index) => (
-            
-            <li key={index} className={styles.modulesList}>
-                {module.activities.length > 0 && (
-                    <div>
-                    <h4 className={styles.moduleName}>Module {module.order} - {module.name} </h4>
-                   
-                        <h4 className={styles.activityListTitle}>Activity List:</h4>
-                        {module.activities?.sort((a,b) => a.order - b.order).map((activity, index) => (
-                            <li key={index} className={styles.activityList}>
-                                <li>{activity.order}: {activity.name} </li>
-                                <li>Activity Type: {activity.type}</li>
-                                {activity.datetime && (
-                                    <li>{new Date(activity.datetime).toString()} </li>
-                                )}
-                                <li>{activity.completed ? 
-                                    (<p className={styles.complete}>COMPLETE</p>) 
-                                    : 
-                                    (<p className={styles.incomplete}>INCOMPLETE</p>)}</li>
-                            </li>
-                        ))}
-                    </div>
-                )}
-            </li>
-            ))}
-        </ul> 
+      <ul>
+        <div className={styles.modulesHeader}>
+          <h2>Modules</h2>
+          <button
+            onClick={() => setShowAllIncomplete(!showAllIncomplete)}
+            className={styles.modulesButton}
+          >
+            SHOW INCOMPLETE MODULES
+          </button>
+        </div>
 
+        {modules
+          ?.sort((a, b) => a.order - b.order)
+          .map((module, index) => (
+            <>
+              {module.activities.length > 0 && (
+                <li key={index} className={styles.modulesList}>
+                  <div>
+                    <h4 className={styles.moduleName}>
+                      Module {module.order} - {module.name}{" "}
+                    </h4>
+
+                    <h4 className={styles.activityListTitle}>Activity List:</h4>
+                    {module.activities
+                      ?.sort((a, b) => a.order - b.order)
+                      .map((activity, index) => (
+                        <li key={index} className={styles.activityList}>
+                          <li>
+                            {activity.order}: {activity.name}{" "}
+                          </li>
+                          <li>Activity Type: {activity.type}</li>
+                          {activity.datetime && (
+                            <li>{new Date(activity.datetime).toString()} </li>
+                          )}
+                          <li>
+                            {activity.completed ? (
+                              <p className={styles.complete}>COMPLETE</p>
+                            ) : (
+                              <p className={styles.incomplete}>INCOMPLETE</p>
+                            )}
+                          </li>
+                        </li>
+                      ))}
+                  </div>
+                </li>
+              )}
+            </>
+          ))}
+      </ul>
     </div>
-    )
+  );
 }
